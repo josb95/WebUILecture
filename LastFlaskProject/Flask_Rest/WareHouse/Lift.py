8d3c8a5d48a1eb557645357327d2362153cf9d66
josb95 <54009650+josb95@users.noreply.github.com>
2019-11-05 19:29:50 +0900

    update

--- LastFlaskProject/Flask_Rest/WareHouse/Lift.py
+++ LastFlaskProject/Flask_Rest/WareHouse/Lift.py
@@ -1,15 +1,18 @@
 class Lift:
-    def __init__(self,x,y):
+    def __init__(self, av, x, y, d, nx, ny):
+        self.available = av
         self.x = x
         self.y = y
+        self.dir = d
+
+        self.next_x = nx
+        self.next_y = ny
+        self.final_x = fx
+        self.final_y = fy
+        
     def start(self):
         pass
     def stop(self):
         pass
     def pause(self):
         pass
-
-
-
-
-
--- LastFlaskProject/WareHouse/Rack.py
+++
@@ -1,9 +0,0 @@
-class Rack :
-    def __init__(self , quantity):
-        self.quantity = quantity
-
-    def put(self):
-        pass
-
-    def get(self):
-        pass
