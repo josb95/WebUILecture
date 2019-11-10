c512df8d04457b8575f3d7e5486f9e3eb2cfbb2b
josb95 <54009650+josb95@users.noreply.github.com>
2019-11-10 16:22:18 +0900

    update

--- LastFlaskProject/Flask_Rest/WareHouse/Rack.py
+++ LastFlaskProject/Flask_Rest/WareHouse/Rack.py
@@ -12,25 +12,21 @@ class Rack :
 
     def put(self, stuff):
         if self.quantity >= max:
+            if self.quantity == 0:
+                self.stuff = stuff
             self.quantity = self.quantity + 1
             if self.quantity == max:
                 self.isFull = True
-        elif self.quantity == 0:
-            self.stuff = stuff
+        elif self.isFull == True:
+            print("Wrong Access")
+        
 
     def get(self):
         if self.quantity != 0:
             self.quantity = self.quantity - 1
             if self.quantity == 0:
                 self.stuff = 'None'
-                write_TimeStamp()
         elif self.quantity == 0:
             print("Wrong Access")
 
-    def write_TimeStamp(self):
-        # 물건 수량이 0이 될 때 타임스탬프를 기록하는 함수
-        pass
-
-    def update_TimeStamp(self):
-        # 일정 주기로 혹은 특정 조건으로 임시로 늘려놓은 물건 수량을 원래대로 되돌려놓는 함수
-        pass
+    
--- LastFlaskProject/Flask_Rest/WareHouse/Storage.py
+++ LastFlaskProject/Flask_Rest/WareHouse/Storage.py
@@ -24,6 +24,34 @@ class Storage:
     def add_Lift(self, liftname, y):
         setattr(self, liftname, L.Lift(y))
         
+    def write_TimeStamp(self):
+        # 물건 수량이 0이 될 때 타임스탬프를 기록하는 함수
+        pass
+
+    def update_TimeStamp(self):
+        # 일정 주기로 혹은 특정 조건으로 임시로 늘려놓은 물건 수량을 원래대로 되돌려놓는 함수
+        pass
+        
+    def received(self):
+        # 입고
+        pass
+    
+    def requisition(self):
+        # 출고
+        pass
+    
+    def bad(self):
+        # 불량
+        pass
+    
+    def calculate_Route(self):
+        # 경로계산
+        pass
+    
+    def send_NextNode(self):
+        # 다음 노드 전송
+        pass
+    
     def init_graph(self):
 
       #  self.g.add_node(1);
