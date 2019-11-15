4312255a36caa119604af42da89dd3606db45a05
josb95 <54009650+josb95@users.noreply.github.com>
2019-11-15 16:05:56 +0900

    test json added

--- LastFlaskProject/Flask_Rest/문법테스트.py
+++ LastFlaskProject/Flask_Rest/문법테스트.py
@@ -4,15 +4,29 @@
 # a = sorted(a)
 # print(a)
 
-counter_list = list()
 
-counter_list.append('abc')
-counter_list.append('bcd')
-counter_list.append('cde')
 
-print(counter_list)
-del counter_list
-try:
-    print(counter_list)
-except NameError:
-    print("정의되지 않은 변수가 호출되었습니다.")
+
+
+
+# counter_list = list()
+
+# counter_list.append('abc')
+# counter_list.append('bcd')
+# counter_list.append('cde')
+
+# print(counter_list)
+# del counter_list
+# try:
+#     print(counter_list)
+# except NameError:
+#     print("정의되지 않은 변수가 호출되었습니다.")
+
+
+test_dict = {'abc': 3, 'bbc': 2, 'cbc': 2}
+print(test_dict)
+print(test_dict['abc'])
+for i in test_dict:
+    key = i
+    value = test_dict[i]
+    print("key : {}, value : {}".format(key, value) )
